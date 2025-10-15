3058c6d2e4ac1b9c0a4cd453323ac68087a42f12
HazZelnutz <49021905+HazZelnutz@users.noreply.github.com>
2025-10-15 12:38:06 +0200

    Increase blinker gap if metric

--- selfdrive/ui/sunnypilot/qt/onroad/hud.cc
+++ selfdrive/ui/sunnypilot/qt/onroad/hud.cc
@@ -801,7 +801,7 @@ void HudRendererSP::drawBlinker(QPainter &p, const QRect &surface_rect) {
 
   const int circleRadius = 60;
   const int arrowLength = 60;
-  const int x_gap = 160;
+  const int x_gap = is_metric ? 170 : 160;
   const int y_offset = 272;
 
   const int centerX = surface_rect.center().x();
