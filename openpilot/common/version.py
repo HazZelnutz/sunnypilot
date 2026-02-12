9fdc63c9805df0b7c490a0bb79ee52af9413c1f4
HazZelnutz <darrellparker10@gmail.com>
2026-02-12 18:33:42 +0100

    Added Hazzelnutz Remotes

--- openpilot/common/version.py
+++ openpilot/common/version.py
@@ -10,9 +10,9 @@ from openpilot.common.basedir import BASEDIR
 from openpilot.common.swaglog import cloudlog
 from openpilot.common.git import get_commit, get_origin, get_branch, get_short_branch, get_commit_date
 
-RELEASE_SP_BRANCHES = ['release-c3', 'release', 'release-tizi', 'release-tici', 'release-tizi-staging', 'release-tici-staging']
-TESTED_SP_BRANCHES = ['staging-c3', 'staging-c3-new', 'staging']
-MASTER_SP_BRANCHES = ['master']
+RELEASE_SP_BRANCHES = ['release-c3', 'release', 'release-tizi', 'release-tici', 'release-tizi-staging', 'release-tici-staging', 'sp-release-hazzel']
+TESTED_SP_BRANCHES = ['staging-c3', 'staging-c3-new', 'staging', 'sp-staging-hazzel']
+MASTER_SP_BRANCHES = ['master', 'sp-master-hazzel']
 RELEASE_BRANCHES = ['release-tizi-staging', 'release-mici-staging', 'release-tizi', 'release-mici', 'nightly']
 TESTED_BRANCHES = RELEASE_BRANCHES + ['devel-staging', 'nightly-dev'] + RELEASE_SP_BRANCHES + TESTED_SP_BRANCHES
 
@@ -107,6 +107,10 @@ class OpenpilotMetadata:
                                           "github.com/sunnypilot/openpilot",
                                           "github.com/sunnyhaibin/sunnypilot",
                                           "github.com/sunnyhaibin/openpilot")
+
+  @property
+  def hazzelnutz_remote(self) -> bool:
+    return self.git_normalized_origin == "github.com/hazzelnutz/sunnypilot"
 
   @property
   def git_normalized_origin(self) -> str:
